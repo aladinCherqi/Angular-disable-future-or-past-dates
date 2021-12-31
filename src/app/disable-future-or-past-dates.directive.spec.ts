9c05e59ba52d980c4d41e557a7dd6737b3fb8d55
Alae eddine cherqi <alaeeddine.cherqi@gmail.com>
2021-12-31 16:51:00 +0100

    first commit

--- src/app/disable-future-or-past-dates.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DisableFutureOrPastDatesDirective } from './disable-future-or-past-dates.directive';
-
-describe('DisableFutureOrPastDatesDirective', () => {
-  it('should create an instance', () => {
-    const directive = new DisableFutureOrPastDatesDirective();
-    expect(directive).toBeTruthy();
-  });
-});
